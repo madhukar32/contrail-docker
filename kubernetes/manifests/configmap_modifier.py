006595a22377c49fb1c8ffa622ac552e35df0637
madhukar32 <mnayakbomman@juniper.net>
2017-07-05 18:52:37 -0700

    Verifies the number from daemonsets status with list of nodes in the configmap

--- kubernetes/manifests/configmap_modifier.py
+++ kubernetes/manifests/configmap_modifier.py
@@ -31,8 +31,9 @@ class ContrailCMModifier(object):
     ''' Modifies the ConfigMap of contrail before starting the contrail
     container '''
 
-    CONFIG_MAP_TAG = '/api/v1/'
+    CORE_V1 = '/api/v1/'
     GLOBAL_SECTION = 'GLOBAL'
+    EXTENSIONS_V1_BETA = '/apis/extensions/v1beta1/'
 
     def __init__(**kwargs):
 
@@ -93,34 +94,49 @@ class ContrailCMModifier(object):
     def _get_config_map(self, namespace=None, name=None):
         ''' Returns json data of the configmap '''
 
-        cm_url  = self._get_url(common=CONFIG_MAP_TAG, namespace=namespace,
+        cm_url  = self._get_url(common=CORE_V1, namespace=namespace,
                                 resource='configmaps', name=name)
 
-        headers = {'Connection': 'Keep-Alive'}
-        headers.update(auth_header)
-
-        resp = requests.get(cm_url, headers=headers, verify=self.verify)
+        resp = requests.get(cm_url, headers=self.headers, verify=self.verify)
 
         if resp.status_code == 200:
             return resp.json()
         else:
             self.logger.critical('response error code: {0}, while getting url:
                                  {1}'.format(resp.status_code, cm_url))
+        sys.exit(1)
+
+    def _get_global_contrail_config(self, global-config-data):
+
+        ''' return global_config string buffer and global-config parser object
+        '''
+
+        global_config_buf = StringIO.StringIO(global-config-data)
+        global_config = ConfigParser.ConfigParser()
+        global_config.readfp(global_config_buf)
+        return global_config_buf, global_config
+
+    def _get_global_config_data(self, cm_data):
+        ''' return global_config_data from the conifgmap '''
+
+        try:
+            global_data = cm_data['data']['global-config']
+        except KeyError:
+            self.logger.critical('ConfigMap {0} does not have global-config
+                                 data'.format(name))
             sys.exit(1)
+        return global_data
+
 
     def update_podip_contrail_cm(self, name=None, namespace=None):
         ''' Updating the podIp in contrail_cm  before starting the provisioning '''
 
         contrail_cm_data = self._get_config_map(name=name, namespace=namespace)
-        try:
-            global_config_buf = StringIO.StringIO(contrail_cm_data['data']['global-config'])
-        except KeyError:
-            self.logger.critical('ConfigMap {0} does not have global-config
-                                 data'.format(name))
-            sys.exit(1)
 
-        global_config = ConfigParser.ConfigParser()
-        global_config.readfp(global_config_buf)
+        global_data = self._get_global_config_data(contrail_cm_data)
+
+        global_config_buf, global_config = self._get_global_contrail_config(global_data)
+
 
         option = self.contrail_role + '_nodes'
         if global_config.has_section(GLOBAL_SECTION):
@@ -139,12 +155,63 @@ class ContrailCMModifier(object):
             self._patch_config_map(name=name, namespace=namespace,
                                              data=contrail_patch_cm)
 
-    def start_contrail_ansible(self, 
+    def _get_desired_number_scheduled(resource_name=None, resource=None,
+                                      namespace=None):
+
+        url  = self._get_url(common=EXTENSIONS_V1BETA1, namespace=namespace,
+                                resource=resource, name=resource_name)
+
+        resp = requests.get(url, headers=self.headers, verify=self.verify)
+
+        if resp.status_code == 200:
+            resource_data = resp.json()
+            return resource_data['status']['desiredNumberScheduled']
+        else:
+            self.logger.critical('response error code: {0}, while getting url:
+                                 {1}'.format(resp.status_code, cm_url))
+            sys.exit(1)
+
+    def _verify_configmap_update(self, **kwargs):
+
+        cm_name = kwargs.get('name', None)
+        cm_namespace = kwargs.get('namespace', None)
+
+        contrail_cm_data = self._get_config_map(name=cm_name, namespace=cm_namespace)
+        global_data = self._get_global_config_data(contrail_cm_data)
+
+        global_config_buf, global_config = self._get_global_contrail_config(global_data)
+
+        resource_name =  'contrail-' + self.contrail_role
+
+        desired_number =
+        self._get_desired_number_scheduled(resource_name=resource_name,
+                                           resource='daemonsets',
+                                           namespace=cm_namespace)
+
+        option = self.contrail_role + '_nodes'
+        contrail_nodes = global_config.get(GLOBAL_SECTION, option)
+        contrail_list = re.split(r',[, ]+', contrail_nodes)
+        if len(contrail_list) == desired_number:
+            return True
+        else:
+            return False
+
+
+    def start_contrail_ansible(self):
+        ''' Start systemd/supervisor process  '''
+
+        for count in range(6):
+            cm_update_successful = self._verify_configmap_update(self, **kwargs)
+            if not cm_update_successful:
+                time.sleep(10)
+                continue
+            else:
+                break
 
     def _patch_config_map(self, name=None, namespace=None,
                       data=None):
 
-        cm_url  = self._get_url(common=CONFIG_MAP_TAG, namespace=namespace,
+        cm_url  = self._get_url(common=CORE_V1, namespace=namespace,
                                 resource='configmaps', name=name)
 
         headers = {'Accept': 'application/json', \
